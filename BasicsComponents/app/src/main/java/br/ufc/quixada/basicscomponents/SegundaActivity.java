e7b6e010227e2a2b7be96466f2eb1e29128a4f07
Bruno da Silva Sales <bruno.sales@lead.fortaleza>
2022-09-17 17:41:36 -0300

    finished, add more one recycler view (total 2)

--- BasicsComponents/app/src/main/java/br/ufc/quixada/basicscomponents/SegundaActivity.java
+++ BasicsComponents/app/src/main/java/br/ufc/quixada/basicscomponents/SegundaActivity.java
@@ -1,33 +1,51 @@
 package br.ufc.quixada.basicscomponents;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import br.ufc.quixada.basicscomponents.model.Contato;
+import br.ufc.quixada.basicscomponents.view.CustomAdapter;
+
 public class SegundaActivity extends AppCompatActivity {
-    private Button goMainActivity, ButtonRec;
+    private Button goMainActivity, ButtonRec, buttonRecycler2;
     private ListView listView;
     ArrayList<Sports> sports = new ArrayList<>();
+    ArrayList<Contato> lista = new ArrayList<>();
+    CustomAdapter adapter;
+    RecyclerView recyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+        adapter = new CustomAdapter( lista );
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView = findViewById(R.id.reclycerView2);
+        recyclerView.setLayoutManager( linearLayoutManager );
+        recyclerView.setAdapter( adapter );
 
         actionsScreenSecondActivity();
+
     }
 
     public void actionsScreenSecondActivity(){
         goMainActivity = findViewById(R.id.button1S);
         ButtonRec = findViewById(R.id.buttonRec);
+        buttonRecycler2 = findViewById(R.id.buttonAddRecycler);
 
         listView = findViewById(R.id.listViewId);
         addValuesToSportList();
@@ -50,6 +68,13 @@ public class SegundaActivity extends AppCompatActivity {
             }
         });
 
+        buttonRecycler2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addValuesToContato();
+            }
+        });
+
         //Questão 14 e 15
         listView.setAdapter(adapter);
     }
@@ -60,6 +85,8 @@ public class SegundaActivity extends AppCompatActivity {
         this.sports.add(new Sports("Jogo de Volei", "Jogando Volei na praia", 25));
     }
 
-
-
+    public void addValuesToContato(){
+        this.lista.add(new Contato("Bruno", "8855", "Rua nova"));
+        adapter.notifyDataSetChanged();
+    }
 }
